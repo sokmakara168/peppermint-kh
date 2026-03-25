7d2e80ab2ad9f598a2f978a27a8ad0f5f2f6d586
sokmakara168 <110456181+sokmakara168@users.noreply.github.com>
2026-03-25 15:51:51 +0700

    Update i18n.js

--- apps/client/i18n.js
+++ apps/client/i18n.js
@@ -1,6 +1,6 @@
 module.exports = {
-  locales: ["en", "da", "de", "es", "fr", "no", "pt", "se", "tl", "is" ,"it", "he", "tr", "hu", "th", "zh-CN"],
-  defaultLocale: "en",
+  locales: ["en", "da", "de", "es", "fr", "no", "pt", "se", "tl", "is", "it", "kh", "he", "tr", "hu", "th", "zh-CN"],
+  defaultLocale: "kh",
   pages: {
     "*": ["peppermint"],
   },
